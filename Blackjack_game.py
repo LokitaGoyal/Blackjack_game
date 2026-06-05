2e32566b7f0c4002b3883442a36c7998f51126db
Lokita Goyal <lokitagoyal9812@gmail.com>
2026-06-05 16:46:42 +0530

    Update Blackjack_game.py

--- Blackjack_game.py
+++ Blackjack_game.py
@@ -14,11 +14,13 @@ cards={"A": 11,
        "J": 10,
        "Q": 10,
        "K": 10}
-deck=[]
-for suit in suits:
-    for rank,value in cards.items():
-        deck.append([rank,suit,value])
-random.shuffle(deck)
+def create_deck():
+    deck = []
+    for suit in suits:
+        for rank, value in cards.items():
+            deck.append([rank, suit, value])
+    random.shuffle(deck)
+    return deck
 
 def total(hand):
     total = 0
@@ -27,12 +29,14 @@ def total(hand):
     return total
 
 def play_game():
-    print("Welcome to Blackjack.")
-    print("Let's play a best of 3 games.")
+    global deck
+    print("🤞😉 Welcome to Blackjack 🃏🃏")
+    print("Let's play a best of 3 games 🎮🎮")
     total_games = 0
     dealer_score = 0
     player_score = 0
-    while total_games < 3:
+    
+    while dealer_score < 2 and player_score < 2:
         player_cards=[]
         dealer_cards=[]
 
@@ -40,54 +44,107 @@ def play_game():
         while len(dealer_cards) != 2:
             dealer_cards.append(deck.pop())
             if len(dealer_cards) == 2:
-                print("Dealer has X &", dealer_cards[1])
+                print("🤖 Dealer has X &", dealer_cards[1])
 
          # Player Cards
         while len(player_cards) != 2:
             player_cards.append(deck.pop())
             if len(player_cards) == 2:
-                print("You have ", player_cards)
-         
-        # Sum of the Dealer cards
+                print("💁 You have ", player_cards)
+
+        # Check for tie (both have Blackjack)
+        if total(player_cards) == 21 and total(dealer_cards) == 21:
+            print("😮😮  It's a tie, both 💁 you and the 🤖 dealer have Blackjack! 😮‍💨😮‍💨")
+            total_games += 1
+            continue
+
+        # Check for Dealer Blackjack or Bust
         if total(dealer_cards) == 21:
+            dealer_score += 1
+            print("🤖 Dealer has Blackjack! 😭😭")
+            total_games += 1
+            continue
+        elif total(dealer_cards) > 21:
+            player_score += 1
+            print("🥳🎉 Dealer busted! You win! 🥳🎉")
+            total_games += 1
+
+        # Check for Player Blackjack or Bust
+        elif total(player_cards) == 21:
+            player_score += 1
+            print("🥳🎉 You have Blackjack! You win! 🥳🎉")
+            total_games += 1
+            continue
+
+        elif total(player_cards) > 21:
             # 2) Add 1 for dealer
             dealer_score += 1
-            print("Dealer has 21 and wins!")
-        elif total(dealer_cards) > 21:
-            # 2) Add 1 for player
-            player_score += 1
-            print("Dealer has busted!")
+            print("You BUSTED! Dealer wins. 😭😭")
+            total_games += 1
 
-        # Sum of the Player cards
         while total(player_cards) < 21:
-            action_taken = str(input("Do you want to stay or hit?  "))
-            if action_taken == "hit":
+
+            if len(deck) < 15:
+                print("Deck is running low. Reshuffling...")
+                deck = create_deck()
+
+            action_taken = str(input("Do 💁 you want to stay or hit?  "))
+
+            if action_taken.lower() == "hit":
                 player_cards.append(deck.pop())
-                print("You now have a total of " + str(total(player_cards)) + " from these cards ", player_cards)
-            else:
-                print("The dealer has a total of " + str(total(dealer_cards)) + " with ", dealer_cards)
-                print("You have a total of " + str(total(player_cards)) + " with ", player_cards)
-                if total(dealer_cards) > total(player_cards):
+                print("💁 You now have a total of",total(player_cards),"from these cards",player_cards)
+
+                if total(player_cards) == 21:
+                    player_score += 1
+                    print("🥳🎉 You have Blackjack! You win! 🥳🎉")
+                    total_games += 1
+                    break
+                elif total(player_cards) > 21:
+                    dealer_score += 1
+                    print("😭😭 You BUSTED! Dealer wins. 😭😭")
+                    total_games += 1
+                    break
+            
+            elif action_taken.lower() == "stay":
+
+                while total(dealer_cards) < 17:
+                       dealer_cards.append(deck.pop())
+                print("🤖 The dealer has a total of " + str(total(dealer_cards)) + " with ", dealer_cards)
+                print("💁 You have a total of " + str(total(player_cards)) + " with ", player_cards)
+                
+                if total(dealer_cards)==21 and total(player_cards) == 21:
+                    print("😮😮 It's a tie, both you and the dealer have Blackjack! 😮‍💨😮‍💨")
+                    total_games += 1
+                    break
+
+                elif total(dealer_cards) > 21:
+                    # 2) Add 1 for player
+                    player_score += 1
+                    print("🥳🎉 Dealer busted! You win! 🥳🎉")
+                    total_games += 1
+                    break
+
+                elif total(dealer_cards) > total(player_cards):
                     # 2) Add 1 for dealer
                     dealer_score += 1
-                    print("Dealer wins!")
+                    print("Dealer wins! 😭😭")
+                    total_games += 1
+                    break
+                
                 else:
                     # 2) Add 1 for player
                     player_score += 1
-                    print("You win!")
+                    print("🥳🎉 You win! 🥳🎉")
+                    total_games += 1
                     break
-
-        if total(player_cards) > 21:
-            # 2) Add 1 for dealer
-            dealer_score += 1
-            print("You BUSTED! Dealer wins.")
-        elif total(player_cards) == 21:
-            # 2) Add 1 for player
-            player_score += 1
-            print("You have BLACKJACK! You Win!! 21")
+           
+            else:
+                print("Invalid input. Please enter 'hit' or 'stay'.")
 
         # Text to inform the player of the status of the game and iterate over the total_games
-        print("Current Best of 3: Dealer - " , str(dealer_score) ,  " times wins & You " + str(player_score) + " times wins")
-        total_games += 1
+        print("Current Best of 3: 🤖 Dealer-", str(dealer_score) ," times wins & 💁 You-" + str(player_score) + " times wins")
+        print("Total games played: " + str(total_games))
+        print("--------------------------------------------------")
 
+deck = create_deck()
 play_game()
